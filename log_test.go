f2c2c4d6f58265aea672343b67d9ace28645b214
Christiaan van Zyl <christiaanvanzyl@gmail.com>
2020-06-14 14:56:49 +0200

    Modified MarshalStack and ConsoleWriter

---
+++ log_test.go
@@ -0,0 +1,16 @@
+package logutil_test
+
+import (
+	"fmt"
+	"github.com/mozey/logutil"
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
+	"testing"
+)
+
+func TestMarshalStack(t *testing.T) {
+	// TODO Capture os.Stdout and verify log output
+	logutil.SetupLogger(true)
+	err := errors.WithStack(fmt.Errorf("testing"))
+	log.Error().Stack().Err(err).Msg("")
+}
